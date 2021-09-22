92973af8366fb2bc43a0a80d25f4ada5fdb68b74
disgustipated <disgustipated@erasethis.net>
2021-09-22 07:58:04 -0400

    Switch to bme280 sensor

--- Thermostat_esp8266/Thermostat_esp8266.ino
+++ Thermostat_esp8266/Thermostat_esp8266.ino
@@ -4,33 +4,35 @@
 #include <ESP8266mDNS.h>
 #include <Wire.h>
 #include <PubSubClient.h> //https://github.com/knolleary/pubsubclient
-#include "DHT.h"
 #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
 #include <Time.h>
 #include <TimeAlarms.h>  //https://github.com/PaulStoffregen/TimeAlarms
 #include <NTPClient.h>   //https://github.com/arduino-libraries/NTPClient
 #include <WiFiUdp.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 //vars
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 #define mqtt_server "6.13.0.120"
-#define mqtt_user "bedroom"
-#define mqtt_password "bedroom"
-#define topic "home/bedroom/temperature"
+#define mqtt_user "spareroom"
+#define mqtt_password "spareroom"
+#define topic "home/spareroom/temperature"
 const int  statusPagePort = 8266;
-const char* SOFTWARE_VERSION = "1.0 Bedroom Thermostat";
-const char* DEVICENAME = "BedroomThermostat"; 
+const char* SOFTWARE_VERSION = "1.0 Spareroom Thermostat";
+const char* DEVICENAME = "SpareroomThermostat"; 
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 //pins
 const int RED_DATA_PIN = 0;
 const int WIFI_INFO_LED_PIN = 2;
-#define DHTPIN 4     // Digital pin connected to the DHT sensor
+const int SENSOR_INFO_LED_PIN = 5;
 const int WIFI_RESET_PIN = 14;
 
 //intervals
 const long CHECK_WIFI_INTERVAL = 30000;
 const long CHECK_MQTT_INTERVAL = 30000;
-const long CHECK_SENSORS_INTERVAL = 5000;
+const long CHECK_SENSORS_INTERVAL = 30000;
 unsigned long prevMillisSensors = 0;
 unsigned long prevMillisWiFi = 0;
 unsigned long prevMillisMQTT = 0;
@@ -50,23 +52,33 @@ ESP8266WebServer server(statusPagePort); //this cant be running on port 80 becau
 WiFiClient espClient;
 PubSubClient client(espClient);
 WiFiManager wifiManager;
-DHT dht(DHTPIN, DHTTYPE);
+Adafruit_BME280 bme; // I2C
 WiFiUDP time_udp;
 NTPClient timeClient(time_udp, ntpServer, ntpOffset * 3600);
 
 void setup() {
+  unsigned bmestatus;  
   Serial.begin(9600);
   pinMode(WIFI_RESET_PIN,INPUT_PULLUP);
   pinMode(WIFI_INFO_LED_PIN,OUTPUT);
   pinMode(RED_DATA_PIN,OUTPUT);
   digitalWrite(RED_DATA_PIN,HIGH);
+  bmestatus = bme.begin();
+  if (!bmestatus) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }    
   //wifiManager.setSTAStaticIPConfig(IPAddress(6,13,0,218), IPAddress(6,13,0,1), IPAddress(255,255,255,0)); //Remove this for DHCP
   wifiManager.setHostname(DEVICENAME);
   if(!wifiManager.autoConnect("ESPSetup", "wifiSetup1")){
     wifiManager.autoConnect("ESPSetup", "wifiSetup1");
   }
   client.setServer(mqtt_server, 1883);
-  dht.begin();
   setupWeb();
 }
 
